568ae829fb26794abcd4354c80f0d2494025380d
name <varunbrahmane30@gmail.com>
2021-07-21 13:12:57 +0530

    [varun] updated: calling user input method

--- MainClass.cs
+++ MainClass.cs
@@ -9,21 +9,8 @@ namespace TicTocToeBoard
             Console.WriteLine("Welcome to Tic Tac Toe Game Board.");
             TicTacToeGame obj = new TicTacToeGame();
             obj.boardCreate();
+            obj.ChoosePlayer();
         }
-        public char ChoosePlayer()
-        {
-            char[] choice = new char[] { 'X', 'O' };
-            Console.WriteLine("Choose X or O ");
-            char playerChoice = char.Parse(Console.ReadLine());
-            if (playerChoice == choice[0])
-            {
-                playerChoice = choice[0];
-            }
-            else
-            {
-                playerChoice = choice[1];
-            }
-            return playerChoice;
-        }
+       
     }
 }
