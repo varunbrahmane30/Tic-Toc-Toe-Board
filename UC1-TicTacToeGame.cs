c82cadb1619d7b72e508cd10f008cfaae7672b22
name <varunbrahmane30@gmail.com>
2021-07-21 11:44:14 +0530

    [varun] added: UC1 added array board

---
+++ UC1-TicTacToeGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTocToeBoard
+{
+    class UC1_TicTacToeGame
+    {
+        static char[] pos = { '0','1','2','3','4','5','6','7','8','9'};
+
+        static void  play()
+        {
+
+        }
+
+    }
+}
