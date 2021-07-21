b07cb3220a1f622a26243a4e3d3e19f0fea1492d
name <varunbrahmane30@gmail.com>
2021-07-21 12:36:48 +0530

    [varun] added: X and O generation

--- UC1-TicTacToeGame.cs
+++ UC1-TicTacToeGame.cs
@@ -6,12 +6,24 @@ namespace TicTocToeBoard
 {
     class UC1_TicTacToeGame
     {
-        static char[] pos = { '0','1','2','3','4','5','6','7','8','9'};
+        static char[] board = { '0','1','2','3','4','5','6','7','8','9'};
 
-        static void  play()
+        public void play()
         {
+            Random random = new Random();
+            int value = random.Next(0, 2);
 
+            if ( value == 1)
+            {
+                Console.WriteLine("X generated");
+            }
+            else
+            {
+                Console.WriteLine("O generated");
+            }
         }
+
+
 
     }
 }
