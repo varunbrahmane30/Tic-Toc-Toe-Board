dd65101ff8d3b090c425d5e5860c81630d5be83e
name <varunbrahmane30@gmail.com>
2021-07-21 13:03:50 +0530

    [varun] added: Board initializes to 0

---
+++ TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTocToeBoard
+{
+    class TicTacToeGame
+    {
+        public char[] boardCreate()
+        {
+            char[] CreateBoard = new char[10];
+            for (int i = 1; i < CreateBoard.Length; i++)
+            {
+                CreateBoard[i] = ' ';
+            }
+            Console.WriteLine("Board Initialized");
+            return CreateBoard;
+        }
+       
+    }
+}
